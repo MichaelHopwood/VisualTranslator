ac3ccfed38a4699fb7bf187465826aa0cc1e8837
Randyll Pandohie <randyllpandohie@Hotmail.com>
2023-04-15 10:54:08 -0400

    Update translateText.py

--- src/translateText.py
+++ src/translateText.py
@@ -9,7 +9,7 @@ def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
 	return response.json()
  
-def translator_es_en(text_to_translate):
+def translate(text_to_translate):
   output = query({
 	"inputs": text_to_translate,
   })
@@ -18,7 +18,7 @@ def translator_es_en(text_to_translate):
 text_to_translate = "Ha abandonado a sus médicos, señora; bajo cuyo prácticas ha perseguido el tiempo con esperanza, y no encuentra otra ventaja en el proceso sino sólo la perdiendo la esperanza por el tiempo."
 
 
-translated= translator_es_en(text_to_translate)
+translated= translate(text_to_translate)
 
 print(translated)
 
