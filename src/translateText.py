a08b3afe2590214420874d1203ac73be959b461d
Joshua White <joshuaderekwhite@gmail.com>
2023-04-19 12:39:22 -0400

    Added sentence parser to avoid max query error.

--- src/translateText.py
+++ src/translateText.py
@@ -1,6 +1,12 @@
 from translate import Translator
+from nltk.tokenize import sent_tokenize
 
 def translate(text):
     translator= Translator(from_lang='es', to_lang='en')
-    translation = translator.translate(text)
+    translation = ''
+
+    # Loop through each sentence to translate
+    sents = sent_tokenize(text)
+    for i in sents:
+        translation += translator.translate(i)
     return translation
