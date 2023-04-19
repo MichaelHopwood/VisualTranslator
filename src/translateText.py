091b40bacc25bee1ec4a2c3727d1da7031327fb6
Randyll Pandohie <randyllpandohie@Hotmail.com>
2023-04-18 22:11:30 -0400

    Updating with ML translate
    Including the ML translations portion

--- src/translateText.py
+++ src/translateText.py
@@ -2,21 +2,12 @@ import requests
 import json
 
 # Translator API
-API_URL = "https://api-inference.huggingface.co/models/Helsinki-NLP/opus-mt-es-en"
-headers = {"Authorization": "Bearer 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"}
 
-def query(payload):
-	response = requests.post(API_URL, headers=headers, json=payload)
-	return response.json()
- 
-def translate(text_to_translate):
-  output = query({
-	"inputs": text_to_translate,
-  })
-  return output
-
-
-if __name__ == "__main__":
-    text_to_translate = "Ha abandonado a sus médicos, señora; bajo cuyo prácticas ha perseguido el tiempo con esperanza, y no encuentra otra ventaja en el proceso sino sólo la perdiendo la esperanza por el tiempo."
-    translated= translate(text_to_translate)
-    print(translated)
+def translate(text):
+    url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=es&tl=en&dt=t&q=" + text
+    response = requests.get(url)
+    if response.status_code == 200:
+        result = response.json()
+        return result[0][0][0]
+    else:
+        return "Error: Unable to translate text"
