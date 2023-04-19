1451d13f273ef1eb8744b2e9ef02b7aa53a2af5b
Joshua White <joshuaderekwhite@gmail.com>
2023-04-19 14:28:31 -0400

    Correction for 500 character limit

--- src/translateText.py
+++ src/translateText.py
@@ -1,5 +1,30 @@
 from translate import Translator
-from nltk.tokenize import sent_tokenize
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+# implemented to avoid 500 char query limit
+def word_boundary(text, char_limit):
+    char_tokens = []
+    current_chunk = []
+    current_length = 0
+
+    for sentence in text:
+        words = word_tokenize(sentence)
+
+        for word in words:
+            word_length = len(word)
+
+            if current_length + word_length + len(current_chunk) <= char_limit:
+                current_chunk.append(word)
+                current_length += word_length
+            else:
+                char_tokens.append(" ".join(current_chunk))
+                current_chunk = [word]
+                current_length = word_length
+
+    if current_chunk:
+        char_tokens.append(" ".join(current_chunk))
+
+    return char_tokens
 
 def translate(text):
     translator= Translator(from_lang='es', to_lang='en')
@@ -8,5 +33,7 @@ def translate(text):
     # Loop through each sentence to translate
     sents = sent_tokenize(text)
     for i in sents:
-        translation += " " + translator.translate(i)
+        # Calculate each word boundary of 500 characters or less
+        for j in word_boundary(i, 500):
+            translation += " " + translator.translate(j)
     return translation
